3f6490f0042744934ea7253f3b9d7c1281994924
Igor Usenko <nmds48@gmail.com>
2013-04-26 00:09:05 +0300

    try to read feed with url fetch api

--- src/main/java/guestbook/GuestbookServlet.java
+++ src/main/java/guestbook/GuestbookServlet.java
@@ -1,13 +1,30 @@
 package guestbook;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import javax.servlet.http.*;
 
 public class GuestbookServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/plain");
-        resp.getWriter().println("Hello, worldy2");
+
+        try {
+            URL url = new URL("http://www.3dnews.ru/news/rss");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                resp.getWriter().println(line);
+            }
+            reader.close();
+
+        } catch (IOException e) {
+            // ...
+        }
     }
     
 }
