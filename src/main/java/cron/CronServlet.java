d2f0d31cecd4dd1057deae0391152fd912fd01c1
Igor Usenko <nmds48@gmail.com>
2013-04-26 21:06:58 +0300

    cron jobs that fetches url and writes to log closes #6

--- src/main/java/cron/CronServlet.java
+++ src/main/java/cron/CronServlet.java
@@ -4,7 +4,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.logging.Logger;
 
 /**
@@ -19,8 +22,27 @@ public class CronServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         LOGGER.info(String.format("doGet [ %d ]", System.currentTimeMillis()));
 
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("UTF-8");
 
-        super.doGet(request, response);
+        try {
+            URL url = new URL("http://www.3dnews.ru/news/rss");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            String line;
+
+            //while ((line = reader.readLine()) != null) {
+            //  response.getWriter().println(line);
+            //}
+
+            line = reader.readLine();
+            line = line == null ? "null" : line;
+
+            response.getWriter().println(line);
+
+            reader.close();
+        } catch (IOException e) {
+            // ...
+        }
     }
 
 }
