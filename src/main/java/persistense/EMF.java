f12622afc56b6ae7a31e4089f1cc4796570bdae6
Igor Usenko <nmds48@gmail.com>
2013-04-27 09:42:01 +0300

    dependencies was corrected, but still in problem with datanucleus enhancement process

--- src/main/java/persistense/EMF.java
+++ src/main/java/persistense/EMF.java
@@ -1,18 +1,21 @@
 package persistense;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 /**
  * Author : Igor Usenko ( igors48@gmail.com )
  * Date : 26.04.13
  */
 public class EMF {
-    /*
-    private static final EntityManagerFactory emfInstance =
-            Persistence.createEntityManagerFactory("transactions-optional");
 
-    private EMF() {}
+    private static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("transactions-optional");
+
+    private EMF() {
+    }
 
     public static EntityManagerFactory get() {
-        return emfInstance;
+        return ENTITY_MANAGER_FACTORY;
     }
-    */
+
 }
--- src/main/java/persistense/Record.java
+++ src/main/java/persistense/Record.java
@@ -1,14 +1,24 @@
 package persistense;
 
+import com.google.appengine.api.datastore.Key;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Author : Igor Usenko ( igors48@gmail.com )
  * Date : 26.04.13
  */
-//@Entity
+@Entity
 public class Record {
-    /*@Id
+
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Key key;
 
-    private String firstName;*/
+    private String value;
+
+
 }
