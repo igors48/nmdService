30a235c1b93f33551fb1f6ffed85a5c13574991e
Igor Usenko <nmds48@gmail.com>
2013-04-28 20:40:33 +0300

    servlet that outputs cron job results from db to page. closes #11

--- src/main/java/guestbook/GuestbookServlet.java
+++ src/main/java/guestbook/GuestbookServlet.java
@@ -1,15 +1,22 @@
 package guestbook;
 
+import persistense.EMF;
+import persistense.Record;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
-import javax.servlet.http.*;
+import java.util.List;
 
 public class GuestbookServlet extends HttpServlet {
 
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException {
         resp.setContentType("text/plain");
 
         try {
@@ -20,11 +27,20 @@ public class GuestbookServlet extends HttpServlet {
             while ((line = reader.readLine()) != null) {
                 resp.getWriter().println(line);
             }
-            reader.close();
 
-        } catch (IOException e) {
-            // ...
+            EntityManager entityManager = EMF.get().createEntityManager();
+
+            TypedQuery<Record> query = entityManager.createQuery("select record from Record record", Record.class);
+            List<Record> result = query.getResultList();
+
+            for (Record record : result) {
+                resp.getWriter().println(record.getValue());
+            }
+
+            reader.close();
+        } catch (Exception e) {
+            throw new ServletException(e);
         }
     }
-    
+
 }
