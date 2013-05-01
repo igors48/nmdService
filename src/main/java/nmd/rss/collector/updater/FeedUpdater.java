95fc73957dbabb08e3b6a69e5695471e04cebc64
Igor Usenko <nmds48@gmail.com>
2013-05-01 06:19:17 +0300

    initial commit

---
+++ src/main/java/nmd/rss/collector/updater/FeedUpdater.java
@@ -0,0 +1,15 @@
+package nmd.rss.collector.updater;
+
+import nmd.rss.collector.feed.Feed;
+
+/**
+ * Author : Igor Usenko ( igors48@gmail.com )
+ * Date : 01.05.13
+ */
+public class FeedUpdater {
+
+    public static void update(final Feed feed) throws FeedUpdaterException {
+
+    }
+
+}
---
+++ src/main/java/nmd/rss/collector/updater/FeedUpdaterException.java
@@ -0,0 +1,8 @@
+package nmd.rss.collector.updater;
+
+/**
+ * Author : Igor Usenko ( igors48@gmail.com )
+ * Date : 01.05.13
+ */
+public class FeedUpdaterException extends Exception {
+}
