dd7917f88590447b941e26e53f8cfaacd18ef956
Igor Usenko <nmds48@gmail.com>
2013-05-22 20:09:38 +0300

    initial commit

---
+++ src/main/java/nmd/rss/collector/controller/Controller.java
@@ -0,0 +1,19 @@
+package nmd.rss.collector.controller;
+
+import java.util.UUID;
+
+/**
+ * Author : Igor Usenko ( igors48@gmail.com )
+ * Date : 22.05.13
+ */
+public class Controller {
+
+    public UUID addFeed(final String feedUrl) throws ControllerException {
+        return null;
+    }
+
+    public UUID removeFeed(final String feedUrl) throws ControllerException {
+        return null;
+    }
+
+}
---
+++ src/main/java/nmd/rss/collector/controller/ControllerException.java
@@ -0,0 +1,8 @@
+package nmd.rss.collector.controller;
+
+/**
+ * Author : Igor Usenko ( igors48@gmail.com )
+ * Date : 22.05.13
+ */
+public class ControllerException extends Exception {
+}
