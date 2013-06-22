9e2d73428302d97d899ffc4dc0cb25d8e9b9c3d0
Igor Usenko <nmds48@gmail.com>
2013-06-22 08:40:58 +0300

    control servlet development

--- src/main/java/nmd/rss/collector/rest/ControlServlet.java
+++ src/main/java/nmd/rss/collector/rest/ControlServlet.java
@@ -1,10 +1,34 @@
 package nmd.rss.collector.rest;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Author : Igor Usenko ( igors48@gmail.com )
  * Date : 22.06.13
  */
 public class ControlServlet extends HttpServlet {
+
+    // GET /feeds -- feeds list
+    // GET /feeds/{feedId} -- get feed
+    @Override
+    protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
+        super.doGet(request, response);
+    }
+
+    // POST /feeds -- add feed
+    @Override
+    protected void doPost(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
+        super.doPost(request, response);
+    }
+
+    // DELETE /feeds/{feedId} -- delete feed
+    @Override
+    protected void doDelete(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
+        super.doDelete(request, response);
+    }
+
 }
