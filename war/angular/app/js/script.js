ffedb349ca03196f4bf695f7076a9054c5e53135
Igor Usenko <nmds48@gmail.com>
2013-07-05 09:34:10 +0300

    experiments

--- war/angular/app/js/script.js
+++ war/angular/app/js/script.js
@@ -1,3 +1,18 @@
-function FeedListCtrl($scope) {
+var application = angular.module('application', ['ngResource']);
+
+application.factory('feeds', function($resource){
+    return $resource('http://localhost\\:8080/security/v01/feeds/', {}, {
+        query: {method:'GET', params:{}}
+    });
+});
+
+function FeedListCtrl($scope, feeds) {
     $scope.feed = 'fd';
+    var feedList = feeds.query(function() {
+        //alert("got it");
+
+        var feedsItem = feedList;
+        $scope.feed = feedsItem.headers.length;
+    } );
 }
+
