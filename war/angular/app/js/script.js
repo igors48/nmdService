552c4872128521761611151e75d8bf47178a9e82
Igor Usenko <nmds48@gmail.com>
2013-07-06 18:48:58 +0300

    feeds add. close #64

--- war/angular/app/js/script.js
+++ war/angular/app/js/script.js
@@ -2,16 +2,31 @@ var application = angular.module('application', ['ngResource']);
 
 application.factory('feeds', function($resource){
     return $resource('/@security.key@/v01/feeds/', {}, {
-        query: {method:'GET', params:{}}
+        'query': {method:'GET', params:{}},
+        'save': {method:'POST'}
     });
 });
 
 function FeedListCtrl($scope, feeds) {
-    $scope.status = 'loading...';
 
-    var feedList = feeds.query(function() {
-        $scope.headers = feedList.headers;
-        $scope.status = feedList.headers.length;
-    } );
+    $scope.loadFeedHeaders = function () {
+        $scope.status = 'loading...';
+
+        var feedList = feeds.query(function() {
+            $scope.headers = feedList.headers;
+            $scope.status = feedList.headers.length;
+        } );
+    };
+
+    $scope.addFeed = function () {
+        $scope.status = 'adding...';
+
+        var response = feeds.save($scope.feedLink, function() {
+            $scope.feedLink = '';
+            $scope.loadFeedHeaders();
+        });
+    };
+
+    $scope.loadFeedHeaders();
 }
 
