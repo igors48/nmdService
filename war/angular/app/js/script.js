6c79c3b60c38a8da5f3145019adff68c1cb85a7a
Igor Usenko <nmds48@gmail.com>
2013-07-20 04:01:17 +0300

    first implementation of backend error handling. close #62

--- war/angular/app/js/script.js
+++ war/angular/app/js/script.js
@@ -9,22 +9,62 @@ application.factory('feeds', function($resource){
 
 function FeedListCtrl($scope, feeds) {
 
+    var serverErrorHandler = function (onContinue) {
+        $scope.status = 'Server error';
+
+        onContinue();
+    }
+
+    var serverResponseHandler = function (response, onSuccess) {
+
+        if (response.status === 'SUCCESS') {
+            onSuccess(response);
+        } else {
+            $scope.status = 'Error : ' + response.message + ' ' + response.hints;
+        }
+
+    }
+
     $scope.loadFeedHeaders = function () {
         $scope.status = 'loading...';
 
-        var feedList = feeds.query(function() {
-            $scope.headers = feedList.headers;
-            $scope.status = feedList.headers.length;
-        } );
+        var feedList = feeds.query(
+            function() {
+                serverResponseHandler(feedList,
+                    function() {
+                        $scope.headers = feedList.headers;
+                        $scope.status = feedList.headers.length;
+                    })
+            },
+            function () {
+                serverErrorHandler(
+                    function () {
+                        $scope.headers = [];
+                    }
+                )
+            }
+        );
     };
 
     $scope.addFeed = function () {
         $scope.status = 'adding...';
 
-        var response = feeds.save($scope.feedLink, function() {
-            $scope.feedLink = '';
-            $scope.loadFeedHeaders();
-        });
+        var response = feeds.save($scope.feedLink,
+            function() {
+                serverResponseHandler(response,
+                    function() {
+                        $scope.feedLink = '';
+                        $scope.loadFeedHeaders();
+                    })
+            },
+            function () {
+                serverErrorHandler(
+                    function () {
+                        $scope.loadFeedHeaders();
+                    }
+                )
+            }
+        );
     };
 
     $scope.loadFeedHeaders();
