6b7ff46523df10ef6e34098a2f8c1708f8ee9f50
Igor Usenko <nmds48@gmail.com>
2013-09-02 10:24:08 +0300

    changes in build process and manual

--- dev_server.bat
+++ dev_server.bat
@@ -1,7 +1,7 @@
 call ant release
 
 if "%ERRORLEVEL%"=="0" (
-    call C:\devTools\appengine-java-sdk\bin\dev_appserver.cmd  --jvm_flag=-Ddatastore.default_high_rep_job_policy_unapplied_job_pct=20 --jvm_flag=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 .\release\war
+    call %GAE_HOME%\bin\dev_appserver.cmd  --jvm_flag=-Ddatastore.default_high_rep_job_policy_unapplied_job_pct=20 --jvm_flag=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 .\release\war
     
     exit
 )
