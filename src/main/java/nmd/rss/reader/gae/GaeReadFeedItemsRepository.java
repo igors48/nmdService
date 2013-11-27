aa2bcaa5a8a91ac9ccd6f768fa4612c33dc1a1d5
Igor Usenko <nmds48@gmail.com>
2013-11-27 07:14:37 +0200

    alarm commit

--- src/main/java/nmd/rss/reader/gae/GaeReadFeedItemsRepository.java
+++ src/main/java/nmd/rss/reader/gae/GaeReadFeedItemsRepository.java
@@ -1,10 +1,16 @@
 package nmd.rss.reader.gae;
 
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
 import nmd.rss.reader.ReadFeedItemsRepository;
 
 import java.util.Set;
 import java.util.UUID;
 
+import static nmd.rss.collector.gae.persistence.GaeRootRepository.DATASTORE_SERVICE;
+import static nmd.rss.collector.gae.persistence.GaeRootRepository.getFeedRootKey;
 import static nmd.rss.collector.util.Assert.assertNotNull;
 import static nmd.rss.collector.util.Assert.assertStringIsValid;
 
@@ -14,22 +20,30 @@ import static nmd.rss.collector.util.Assert.assertStringIsValid;
  */
 public class GaeReadFeedItemsRepository implements ReadFeedItemsRepository {
 
+    private static final String KIND = "ReadFeedItems";
+
     @Override
-    public Set<String> load(UUID feedId) {
+    public Set<String> load(final UUID feedId) {
         assertNotNull(feedId);
+
+        final Key feedRootKey = getFeedRootKey(feedId);
+        final Query query = new Query(KIND).setAncestor(feedRootKey);
+        final PreparedQuery preparedQuery = DATASTORE_SERVICE.prepare(query);
+
+        final Entity entity = preparedQuery.asSingleEntity();
 
         return null;
     }
 
     @Override
-    public void store(UUID feedId, String itemId) {
+    public void store(final UUID feedId, final String itemId) {
         assertNotNull(feedId);
         assertStringIsValid(itemId);
 
     }
 
     @Override
-    public void delete(UUID feedId) {
+    public void delete(final UUID feedId) {
         assertNotNull(feedId);
 
     }
---
+++ src/main/java/nmd/rss/reader/gae/ReadFeedIdSetHelper.java
@@ -0,0 +1,8 @@
+package nmd.rss.reader.gae;
+
+/**
+ * Author : Igor Usenko ( igors48@gmail.com )
+ * Date : 26.11.13
+ */
+public class ReadFeedIdSetHelper {
+}
