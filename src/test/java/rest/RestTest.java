e89133e5ee4e368eb8fc687d665d95580dd05b00
igors48 <nmds48@gmail.com>
2013-11-28 16:05:52 +0200

    first working variant

--- src/test/java/rest/RestTest.java
+++ src/test/java/rest/RestTest.java
@@ -1,8 +1,17 @@
 package rest;
 
+import com.jayway.restassured.RestAssured;
+import org.junit.Test;
+
 /**
  * User: igu
  * Date: 28.11.13
  */
 public class RestTest {
+
+    @Test
+    public void smoke() {
+        RestAssured.get("/");
+    }
+
 }
