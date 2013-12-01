4bb8e95f2c5188e8a5c389160006c0bbbdbce811
Igor Usenko <nmds48@gmail.com>
2013-12-01 10:40:28 +0200

    tests for updates servlet

--- src/test/java/rest/FeedUpdateTest.java
+++ src/test/java/rest/FeedUpdateTest.java
@@ -1,8 +1,11 @@
 package rest;
 
+import nmd.rss.collector.rest.responses.FeedIdResponse;
+import nmd.rss.collector.rest.responses.FeedMergeReportResponse;
 import org.junit.Test;
 
 import static nmd.rss.collector.error.ErrorCode.NO_SCHEDULED_TASK;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Author : Igor Usenko ( igors48@gmail.com )
@@ -17,4 +20,18 @@ public class FeedUpdateTest extends AbstractRestTest {
         assertErrorResponse(response, NO_SCHEDULED_TASK);
     }
 
+    @Test
+    public void whenCurrentFeedExistsThenItUpdatesAndReportReturns() {
+        final FeedIdResponse feedIdResponse = addFirstFeed();
+
+        final String response = updateCurrentFeed();
+        final FeedMergeReportResponse report = GSON.fromJson(response, FeedMergeReportResponse.class);
+
+        final FeedMergeReportResponse expected = new FeedMergeReportResponse(FIRST_FEED_URL, feedIdResponse.getFeedId(), 0, 100, 0);
+
+        assertEquals(expected, report);
+    }
+
+    //more than one update sequentally
+    //concrete feed updates
 }
