640d4ccb3299ddc3b7977f55de4971cbf9687821
igors48 <nmds48@gmail.com>
2013-12-05 18:47:39 +0200

    initial commit

--- src/main/java/nmd/rss/collector/gae/persistence/GaeRootRepository.java
+++ src/main/java/nmd/rss/collector/gae/persistence/GaeRootRepository.java
@@ -3,9 +3,13 @@ package nmd.rss.collector.gae.persistence;
 import com.google.appengine.api.datastore.*;
 import nmd.rss.collector.Transactions;
 
+import java.util.List;
 import java.util.UUID;
 
+import static com.google.appengine.api.datastore.FetchOptions.Builder.withLimit;
+import static java.lang.Integer.MAX_VALUE;
 import static nmd.rss.collector.util.Assert.assertNotNull;
+import static nmd.rss.collector.util.Assert.assertStringIsValid;
 
 /**
  * User: igu
@@ -25,10 +29,51 @@ public class GaeRootRepository implements Transactions {
         return DATASTORE_SERVICE.beginTransaction();
     }
 
+    // TODO try to make it private
     public static Key getFeedRootKey(final UUID feedId) {
         assertNotNull(feedId);
 
         return KeyFactory.createKey(FEEDS_ROOT_KEY, FEED_ENTITY_KIND, feedId.toString());
+    }
+
+    public static Entity loadEntity(final UUID feedId, final String kind, final boolean keysOnly) {
+        assertNotNull(feedId);
+        assertStringIsValid(kind);
+
+        final PreparedQuery preparedQuery = prepareQuery(feedId, kind, keysOnly);
+
+        return preparedQuery.asSingleEntity();
+    }
+
+    public static List<Entity> loadEntities(final UUID feedId, final String kind, final boolean keysOnly) {
+        assertNotNull(feedId);
+        assertStringIsValid(kind);
+
+        final PreparedQuery preparedQuery = prepareQuery(feedId, kind, keysOnly);
+
+        return preparedQuery.asList(withLimit(MAX_VALUE));
+    }
+
+    public static void deleteEntity(final UUID feedId, final String kind) {
+        assertNotNull(feedId);
+        assertStringIsValid(kind);
+
+        final Entity victim = loadEntity(feedId, kind, true);
+
+        if (victim != null) {
+            DATASTORE_SERVICE.delete(victim.getKey());
+        }
+    }
+
+    private static PreparedQuery prepareQuery(final UUID feedId, final String kind, final boolean keysOnly) {
+        final Key feedRootKey = getFeedRootKey(feedId);
+        final Query query = new Query(kind).setAncestor(feedRootKey);
+
+        if (keysOnly) {
+            query.setKeysOnly();
+        }
+
+        return DATASTORE_SERVICE.prepare(query);
     }
 
 }
