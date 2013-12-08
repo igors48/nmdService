d025a66f601295d2f4fe7e2ba94f6e51847012ba
Igor Usenko <nmds48@gmail.com>
2013-12-08 09:53:41 +0200

    dev server settings for local net visibility

--- dev_server.bat
+++ dev_server.bat
@@ -1,7 +1,7 @@
 call ant release
 
 if "%ERRORLEVEL%"=="0" (
-    call %GAE_HOME%\bin\dev_appserver.cmd  --jvm_flag=-Ddatastore.default_high_rep_job_policy_unapplied_job_pct=0 --jvm_flag=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 .\release\war
+    call %GAE_HOME%\bin\dev_appserver.cmd  --jvm_flag=-Ddatastore.default_high_rep_job_policy_unapplied_job_pct=0 --jvm_flag=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 --address=0.0.0.0 .\release\war
     
     exit
 )
