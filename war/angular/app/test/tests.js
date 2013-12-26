041bd0b8bf1ba476d17c8fd2c52587b2e7549459
igors48 <nmds48@gmail.com>
2013-12-26 16:04:33 +0200

    first working variant without assertions

--- war/angular/app/test/tests.js
+++ war/angular/app/test/tests.js
@@ -1,26 +1,74 @@
-http://sravi-kiran.blogspot.com/2013/06/UnitTestingAngularJsControllerUsingQUnitAndSinon.html
-//TODO it needs to disable or mock the blockUI service
+//http://sravi-kiran.blogspot.com/2013/06/UnitTestingAngularJsControllerUsingQUnitAndSinon.html
 
-var injector = angular.injector(['ngMock', 'ng', 'application']);
+//Mocks
+var windowMock, httpBackend, _shoppingData;
+var feedsMock, locationMock, readsMock, blockUiMock;
+//Injector
+var injector;
 
-var init = {
+//Controller
+var ctrl;
+
+//Scope
+var ctrlScope;
+
+//Data
+var storedItems;
+
+module('tests', {
     setup: function () {
-        this.$scope = injector.get('$rootScope').$new();
-    }
-};
+        
+        var appMocks = angular.module("appMocks", []);
 
-module('tests', init);
+        appMocks.config(function ($provide) {
+            $provide.decorator('$httpBackend', angular.mock.e2e.$httpBackendDecorator);
+        });
+
+        injector = angular.injector(['ng', 'application', 'appMocks']);
+        
+        httpBackend = injector.get('$httpBackend');
+        httpBackend.expectGET('/@security.key@/v01/reads').respond('{"reports":[{"feedId":"8dc35df0-0027-45a7-935f-20b54e6d9f98","feedTitle":"Bash.im","read":0,"notRead":100,"addedFromLastVisit":0,"topItemId":"9931c369-e267-4368-93ea-c89e55fbcced","topItemLink":"http://bash.im/quote/426086"}],"status":"SUCCESS"}');
+
+        windowMock = { location: { href: ""} };
+
+        feedsMock = injector.get('feeds');
+        sinon.spy(feedsMock, 'query');
+
+
+        locationMock = {};//injector.get('$location');
+        //sinon.spy(locationMock, 'path');
+
+        readsMock = injector.get('reads');
+
+        blockUiMock = injector.get('blockUi');
+        sinon.stub(blockUiMock, "block");
+        sinon.stub(blockUiMock, "unblock");
+
+        ctrlScope = injector.get('$rootScope').$new();
+
+        ctrl = injector.get('$controller')('feedListCtrl', { $scope: ctrlScope, $window: windowMock, $location: locationMock, feeds: feedsMock, reads: readsMock, blockUi: blockUiMock});
+    },
+
+    teardown: function () {
+    	/*
+        feedsMock.setCartItems.restore();
+
+        _shoppingData.getAllItems.restore();
+        _shoppingData.addAnItem.restore();
+        _shoppingData.removeItem.restore();
+        */
+    }	
+});
 
 test("hello test", function () {
-	var self = this;
+	httpBackend.flush();
 
-	var $controller = injector.get('$controller');
+	ctrlScope.loadReadsReport();
 
-	$controller('feedListCtrl', {
-        $scope: self.$scope
-    });
+    httpBackend.expectGET('/@security.key@/v01/reads').respond('{"reports":[{"feedId":"8dc35df0-0027-45a7-935f-20b54e6d9f98","feedTitle":"Bash.im","read":0,"notRead":100,"addedFromLastVisit":0,"topItemId":"9931c369-e267-4368-93ea-c89e55fbcced","topItemLink":"http://bash.im/quote/426086"}],"status":"SUCCESS"}');
 
-	this.$scope.dummy();
+	//httpBackend.flush();
 
 	ok( 1 == "1", "Passed!" );
 });
+
