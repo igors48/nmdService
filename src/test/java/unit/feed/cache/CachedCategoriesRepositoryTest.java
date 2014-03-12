1a4873cc96a7295b4f1ba7af6f633fc0e6b7dcae
Igor Usenko <nmds48@gmail.com>
2014-03-12 21:19:35 +0200

    cached category repository with tests

---
+++ src/test/java/unit/feed/cache/CachedCategoriesRepositoryTest.java
@@ -0,0 +1,84 @@
+package unit.feed.cache;
+
+import nmd.rss.reader.CachedCategoriesRepository;
+import nmd.rss.reader.Category;
+import org.junit.Before;
+import org.junit.Test;
+import unit.feed.controller.CategoriesRepositoryStub;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import static org.junit.Assert.*;
+
+/**
+ * Author : Igor Usenko ( igors48@gmail.com )
+ * Date : 12.03.14
+ */
+public class CachedCategoriesRepositoryTest {
+
+    private static final String CATEGORY_ID = UUID.randomUUID().toString();
+    private static final Category STORED = new Category(CATEGORY_ID, "stored");
+    private static final Category CACHED = new Category(CATEGORY_ID, "cached");
+
+    private CategoriesRepositoryStub categoriesRepositoryStub;
+    private CacheStub cache;
+
+    private CachedCategoriesRepository cachedCategoriesRepository;
+
+    @Before
+    public void setUp() {
+        this.cache = new CacheStub();
+        this.cache.put(CachedCategoriesRepository.KEY, new HashSet<Category>() {{
+            add(CACHED);
+        }});
+
+        this.categoriesRepositoryStub = new CategoriesRepositoryStub();
+        this.categoriesRepositoryStub.store(STORED);
+
+        this.cachedCategoriesRepository = new CachedCategoriesRepository(this.categoriesRepositoryStub, this.cache);
+    }
+
+    @Test
+    public void whenCategoryWasDeletedThenCacheWasCleared() {
+        this.cachedCategoriesRepository.delete(CATEGORY_ID);
+
+        assertTrue(this.cache.isEmpty());
+    }
+
+    @Test
+    public void whenCategoryWasStoredThenCacheWasCleared() {
+        this.cachedCategoriesRepository.store(STORED);
+
+        assertTrue(this.cache.isEmpty());
+    }
+
+    @Test
+    public void whenCategoriesListWasCachedThenItsWillBeReturnedFromCache() {
+        final Set<Category> categories = this.cachedCategoriesRepository.loadAll();
+
+        assertEquals(1, categories.size());
+        assertEquals(CACHED, categories.iterator().next());
+    }
+
+    @Test
+    public void whenCategoriesListWasNotCachedThenItsWillBeReturnedFromDatastore() {
+        this.cache.clear();
+
+        final Set<Category> categories = this.cachedCategoriesRepository.loadAll();
+
+        assertEquals(1, categories.size());
+        assertEquals(STORED, categories.iterator().next());
+    }
+
+    @Test
+    public void whenCategoriesListWasNotCachedThenCacheIsUpdatedWhileLoading() {
+        this.cache.clear();
+
+        this.cachedCategoriesRepository.loadAll();
+
+        assertFalse(this.cache.isEmpty());
+    }
+
+}
---
+++ src/test/java/unit/feed/controller/CategoriesRepositoryStub.java
@@ -0,0 +1,41 @@
+package unit.feed.controller;
+
+import nmd.rss.reader.CategoriesRepository;
+import nmd.rss.reader.Category;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Author : Igor Usenko ( igors48@gmail.com )
+ * Date : 12.03.14
+ */
+public class CategoriesRepositoryStub implements CategoriesRepository {
+
+    private final Map<String, Category> categories;
+
+    public CategoriesRepositoryStub() {
+        this.categories = new HashMap<>();
+    }
+
+    @Override
+    public void store(final Category category) {
+        this.categories.put(category.uuid, category);
+    }
+
+    @Override
+    public Set<Category> loadAll() {
+        final Set<Category> categorySet = new HashSet<>();
+        categorySet.addAll(this.categories.values());
+
+        return categorySet;
+    }
+
+    @Override
+    public void delete(final String categoryId) {
+        this.categories.remove(categoryId);
+    }
+
+}
