486711e452a5a42f676d1c45065aea4648294bd6
igors48 <nmds48@gmail.com>
2014-07-16 12:48:24 +0300

    build script edited

--- dev_server.bat
+++ dev_server.bat
@@ -1,4 +1,4 @@
-call ant release
+call ant build
 
 if "%ERRORLEVEL%"=="0" (
     call %GAE_HOME%\bin\dev_appserver.cmd  --jvm_flag=-Ddatastore.default_high_rep_job_policy_unapplied_job_pct=0 --jvm_flag=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 --address=0.0.0.0 .\release\war
