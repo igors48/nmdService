87bdada335c72a07c409ae896678b204bb0d5594
igors48 <nmds48@gmail.com>
2014-07-24 17:20:43 +0300

    work with category editor

--- war/ionic/js/edit-category-controller.js
+++ war/ionic/js/edit-category-controller.js
@@ -11,6 +11,46 @@ controllers.controller('editCategoryController',
     		$state.go('categories');
     	};
 
+        $scope.onRenameChosen = function () {
+            $scope.category.newName = $scope.category.name;
+            $scope.mode = 'rename';
+        };
+
+        $scope.onDeleteChosen = function () {
+            $scope.mode = 'delete';
+        };
+
+        $scope.onCancelChosen = function () {
+            $scope.mode = 'choose';
+        };
+
+        $scope.renameCategory = function () {
+            $ionicLoading.show({
+                template: 'Renaming category...'
+            });
+
+            categories.update(
+                { 
+                    categoryId: $stateParams.id
+                },
+                $scope.category.newName,
+                onUpdateCategoryCompleted,
+                onServerFault);
+        };
+
+        $scope.deleteCategory = function () {
+            $ionicLoading.show({
+                template: 'Deleting category...'
+            });
+
+            categories.delete(
+                { 
+                    categoryId: $stateParams.id
+                },
+                onDeleteCategoryCompleted,
+                onServerFault);
+        };
+
         var loadCategoryReport = function () {
             $ionicLoading.show({
                 template: 'Loading category...'
@@ -34,11 +74,42 @@ controllers.controller('editCategoryController',
             }
 
             $scope.showUi = true;
+            $scope.mode = 'choose';
 
             $scope.category = { 
                 name: response.report.name,
                 feeds: response.report.feedReports.length
             };
+        };
+
+        var onDeleteCategoryCompleted = function (response) {
+            $ionicLoading.hide();
+
+            if (response.status !== 'SUCCESS') {
+                $scope.utilities.showError($ionicPopup, response);
+
+                return;
+            }
+
+            $ionicPopup.alert({
+                    title: 'Information',
+                    template: 'Category is deleted.'
+            }).then($scope.backToCategories);
+        };
+
+        var onUpdateCategoryCompleted = function (response) {
+            $ionicLoading.hide();
+
+            if (response.status !== 'SUCCESS') {
+                $scope.utilities.showError($ionicPopup, response);
+
+                return;
+            }
+
+            $ionicPopup.alert({
+                    title: 'Information',
+                    template: 'Category is updated.'
+            }).then($scope.backToCategories);
         };
 
         var onServerFault = function () {
