ec498e5ea166bf18b391ad4df496457d4a583fd9
igors48 <nmds48@gmail.com>
2014-11-21 15:12:45 +0200

    feed update task disabled

--- src/main/java/nmd/orb/services/UpdatesService.java
+++ src/main/java/nmd/orb/services/UpdatesService.java
@@ -91,8 +91,8 @@ public class UpdatesService extends AbstractService {
             final FeedItemsMergeReport mergeReport = FeedItemsMerger.merge(olds, feed.items, updateTask.maxFeedItemsCount);
             final List<FeedItem> addedAndRetained = mergeReport.getAddedAndRetained();
 
-            final FeedUpdateTask updatedTask = updateTask.updateStatistic(mergeReport.added.size());
-            this.feedUpdateTaskRepository.updateTask(updatedTask);
+            //final FeedUpdateTask updatedTask = updateTask.updateStatistic(mergeReport.added.size());
+            //this.feedUpdateTaskRepository.updateTask(updatedTask);
 
             final boolean needsToStore = !(mergeReport.added.isEmpty() && mergeReport.removed.isEmpty());
 
