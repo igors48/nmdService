5c1d4714c370cc08ddeeddc6536e5d6a5d3bcfc1
igors48 <nmds48@gmail.com>
2014-11-21 16:44:27 +0200

    #185 initial

---
+++ src/main/java/nmd/orb/http/servlets/BackupServlet.java
@@ -0,0 +1,17 @@
+package nmd.orb.http.servlets;
+
+import nmd.orb.http.BaseServlet;
+
+/**
+ * @author : igu
+ */
+public class BackupServlet extends BaseServlet {
+
+    /*
+        response.getOutputHeaders().putSingle("Content-Disposition", format("attachment;filename=\"%s\"", file.getFileName()));
+        response.getOutputHeaders().putSingle("Content-Transfer-Encoding", "Binary");
+        response.getOutputHeaders().putSingle("Content-Type", file.getFileType());
+        response.getOutputHeaders().putSingle("Content-Length", file.getSize());
+
+     */
+}
