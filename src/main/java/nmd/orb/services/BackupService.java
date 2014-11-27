4274eec317c1461a2423fd3fbf3593bcebf067ab
Igor Usenko <nmds48@gmail.com>
2014-11-27 23:00:35 +0200

    #185 backup service interface draft

---
+++ src/main/java/nmd/orb/services/BackupService.java
@@ -0,0 +1,23 @@
+package nmd.orb.services;
+
+/**
+ * Created by igor on 27.11.2014.
+ */
+public class BackupService {
+
+    public void schedule() {
+    }
+
+    ;
+
+    public void status() {
+    }
+
+    ;
+
+    public void reject() {
+    }
+
+    ;
+
+}
