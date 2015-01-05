f4a80c298b71bd20ccb2b06ae9f4cac3388eebdd
igors48 <nmds48@gmail.com>
2015-01-05 14:49:31 +0200

    #245 client part

--- war/ionic/js/reset-service-controller.js
+++ war/ionic/js/reset-service-controller.js
@@ -2,14 +2,40 @@
 
 controllers.controller('resetServiceController',
 
-    function ($scope, $state) {
+    function ($scope, $state, $ionicLoading, $ionicPopup, reset) {
+
+        $scope.utilities = AppUtilities.utilities;
 
         $scope.onBackToAdminConsole = function () {
             $state.go('admin-console');
         };
 
         $scope.onResetConfirmed = function () {
-            alert('onResetConfirmed');
+            $ionicLoading.show({
+                template: 'Resetting server...'
+            });
+
+            reset.reset(
+                {}, 
+                onResetSuccess, 
+                onServerFault);        
+        };
+
+        var onResetSuccess = function (response) {
+            $ionicLoading.hide();
+
+            if (response.status === 'SUCCESS') {
+                $ionicPopup.alert({
+                    title: 'Information',
+                    template: 'Server reset complete.'
+                });
+            } else {
+                $scope.utilities.showError($ionicPopup, response);
+            }
+        };
+
+        var onServerFault = function () {
+            $scope.utilities.showFatalError($ionicPopup, $ionicLoading);        
         };
 
     }
