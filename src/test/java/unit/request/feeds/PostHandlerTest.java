a5633cdff0575b56be7cf7699860372ab504333e
Igor Usenko <nmds48@gmail.com>
2015-01-17 10:29:14 +0200

    #199 feeds post handler test

---
+++ src/test/java/unit/request/feeds/PostHandlerTest.java
@@ -0,0 +1,17 @@
+package unit.request.feeds;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by igor on 17.01.2015.
+ */
+public class PostHandlerTest {
+
+
+    @Before
+    public void setUp() {
+
+
+    }
+}
