c09aff7d2a03ee6af76cacec943759f2f398ecdb
Igor Usenko <nmds48@gmail.com>
2015-01-31 14:27:12 +0200

    #199 feeds put handler test

--- src/test/java/unit/handler/feeds/PutHandlerTest.java
+++ src/test/java/unit/handler/feeds/PutHandlerTest.java
@@ -1,13 +1,16 @@
 package unit.handler.feeds;
 
+import nmd.orb.error.ErrorCode;
 import nmd.orb.http.servlets.feeds.FeedsServletPutRequestHandler;
 import nmd.orb.http.wrappers.FeedsServiceWrapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
+import unit.feed.parser.FeedHeaderBuilderTest;
 
 import java.util.UUID;
 
+import static unit.handler.Tools.assertError;
 import static unit.handler.Tools.call;
 
 /**
@@ -32,6 +35,30 @@ public class PutHandlerTest {
         call(this.handler, feedId.toString(), title);
 
         Mockito.verify(this.feedsServiceWrapper).updateFeedTitle(feedId, title);
+    }
+
+    @Test
+    public void whenNoFeedIdThenErrorReturns() {
+        assertError(
+                call(this.handler, ""),
+                ErrorCode.INVALID_FEED_ID
+        );
+    }
+
+    @Test
+    public void whenFeedIdIsInvalidThenErrorReturns() {
+        assertError(
+                call(this.handler, "invalid"),
+                ErrorCode.INVALID_FEED_ID
+        );
+    }
+
+    @Test
+    public void whenFeedTitleIsNotValidThenErrorReturns() {
+        assertError(
+                call(this.handler, UUID.randomUUID().toString(), FeedHeaderBuilderTest.LONG_STRING),
+                ErrorCode.INVALID_FEED_TITLE
+        );
     }
 
 }
