d772d1bbf02d0d9deb67e2798019796723d8f7f5
Igor Usenko <nmds48@gmail.com>
2015-02-20 08:34:17 +0200

    cleanups

--- src/main/java/nmd/orb/gae/cache/MemCache.java
+++ src/main/java/nmd/orb/gae/cache/MemCache.java
@@ -4,7 +4,8 @@ import com.google.appengine.api.memcache.MemcacheService;
 import com.google.appengine.api.memcache.MemcacheServiceFactory;
 import nmd.orb.repositories.Cache;
 
-import static nmd.orb.util.Assert.assertNotNull;
+import static nmd.orb.util.Assert.guard;
+import static nmd.orb.util.Parameter.notNull;
 
 /**
  * Author : Igor Usenko ( igors48@gmail.com )
@@ -15,26 +16,25 @@ public enum MemCache implements Cache {
     INSTANCE;
 
     private final MemcacheService cache = MemcacheServiceFactory.getMemcacheService();
-    ;
 
     @Override
     public synchronized void put(final Object key, final Object object) {
-        assertNotNull(key);
-        assertNotNull(object);
+        guard(notNull(key));
+        guard(notNull(object));
 
         cache.put(key, object);
     }
 
     @Override
     public synchronized Object get(final Object key) {
-        assertNotNull(key);
+        guard(notNull(key));
 
         return cache.get(key);
     }
 
     @Override
     public synchronized boolean delete(final Object key) {
-        assertNotNull(key);
+        guard(notNull(key));
 
         return cache.delete(key);
     }
