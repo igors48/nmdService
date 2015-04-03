189fc7c2b84e66bdd306a9db93748cb9438e1b6e
igors48 <nmds48@gmail.com>
2015-04-03 11:29:54 +0300

    #199 categories get handler test

--- src/test/java/unit/handler/categories/GetHandlerTest.java
+++ src/test/java/unit/handler/categories/GetHandlerTest.java
@@ -1,7 +1,7 @@
 package unit.handler.categories;
 
 import nmd.orb.error.ErrorCode;
-import nmd.orb.http.servlets.categories.CategoriesServletDeleteRequestHandler;
+import nmd.orb.http.servlets.categories.CategoriesServletGetRequestHandler;
 import nmd.orb.http.wrappers.CategoriesServiceWrapper;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -12,21 +12,22 @@ import static unit.handler.Tools.call;
 /**
  * @author : igu
  */
-public class DeleteHandlerTest {
+public class GetHandlerTest {
 
     private CategoriesServiceWrapper categoriesServiceWrapper;
-    private CategoriesServletDeleteRequestHandler handler;
+    private CategoriesServletGetRequestHandler handler;
 
     private void setUp() {
         this.categoriesServiceWrapper = Mockito.mock(CategoriesServiceWrapper.class);
-        this.handler = new CategoriesServletDeleteRequestHandler(this.categoriesServiceWrapper);
+        this.handler = new CategoriesServletGetRequestHandler(this.categoriesServiceWrapper);
     }
 
     @Test
     public void routingTest() {
 
         setUp();
-        assertError(call(this.handler, "/"), ErrorCode.INVALID_CATEGORY_ID);
+        call(this.handler, "/");
+        Mockito.verify(this.categoriesServiceWrapper).getCategoriesReport();
         Mockito.verifyNoMoreInteractions(this.categoriesServiceWrapper);
 
         setUp();
@@ -35,7 +36,7 @@ public class DeleteHandlerTest {
 
         setUp();
         call(this.handler, "/fb5ea2da-2f60-4c11-9232-80bf50d49cf4");
-        Mockito.verify(this.categoriesServiceWrapper).deleteCategory("fb5ea2da-2f60-4c11-9232-80bf50d49cf4");
+        Mockito.verify(this.categoriesServiceWrapper).getCategoryReport("fb5ea2da-2f60-4c11-9232-80bf50d49cf4");
         Mockito.verifyNoMoreInteractions(this.categoriesServiceWrapper);
 
     }
